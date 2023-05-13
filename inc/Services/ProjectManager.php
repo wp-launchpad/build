86b3c78b79f6e3bc2dd9efa7614efef6aca19b24
cyrille <coquardcyr@gmail.com>
2023-05-14 01:47:04 +0200

    Added version updater for files

--- inc/Services/ProjectManager.php
+++ inc/Services/ProjectManager.php
@@ -4,6 +4,7 @@ namespace LaunchpadBuild\Services;
 
 use Ahc\Cli\Helper\Shell;
 use LaunchpadBuild\Entities\Version;
+use LaunchpadCLI\Entities\Configurations;
 use League\Flysystem\Filesystem;
 
 class ProjectManager
@@ -14,16 +15,35 @@ class ProjectManager
     protected $filesystem;
 
     /**
+     * @var Configurations
+     */
+    protected $configurations;
+
+    const PARAMETERS_PATH = 'configs'. DIRECTORY_SEPARATOR . 'parameters.php';
+
+    /**
      * @param Filesystem $filesystem
      */
-    public function __construct(Filesystem $filesystem)
+    public function __construct(Filesystem $filesystem, Configurations $configurations)
     {
         $this->filesystem = $filesystem;
+        $this->configurations = $configurations;
     }
 
 
     public function get_plugin_name() : string {
+        $namespace = $this->configurations->getBaseNamespace();
+        if(strlen($namespace) === 0) {
+            return '';
+        }
+        $namespace = rtrim($namespace, '\\');
+        $lastNamespaceSegment = substr($namespace, strrpos($namespace, '\\') + 1);
 
+        $words = preg_split('/(?=[A-Z])/', $lastNamespaceSegment, -1, PREG_SPLIT_NO_EMPTY);
+
+        $projectName = $namespace[0] . implode('-', $words);
+
+        return strtolower($projectName);
     }
 
     public function run_regular_install(string $plugin_directory) {
@@ -39,7 +59,39 @@ class ProjectManager
     }
 
     public function update_version(Version $version = null) {
+        $new_version = is_null($version) ? '1.0.0' : $version->get_value();
+        if(! $this->filesystem->has(self::PARAMETERS_PATH)) {
+            return;
+        }
 
+        $content = $this->filesystem->read(self::PARAMETERS_PATH);
+        if(! preg_match('/[\'"]plugin_version[\'"]\s*=>\s*[\'"](?<version>[.0-9]*)[\'"]/', $content, $match)) {
+            return;
+        }
+        $version = $match['version'];
+
+        $full_match = $match[0];
+        $full_match_new_version = str_replace($version, $new_version, $full_match);
+        $content = str_replace($full_match, $full_match_new_version, $content);
+
+        $this->filesystem->update(self::PARAMETERS_PATH, $content);
+
+        $main_file = $this->get_plugin_name() . '.php';
+
+        if(! $this->filesystem->has($main_file)) {
+            return;
+        }
+        $content = $this->filesystem->read($main_file);
+        if(! preg_match('/\*\bVersion:\b(?<version>[.0-9]*)/', $content, $match)) {
+            return;
+        }
+        $version = $match['version'];
+
+        $full_match = $match[0];
+        $full_match_new_version = str_replace($version, $new_version, $full_match);
+        $content = str_replace($full_match, $full_match_new_version, $content);
+
+        $this->filesystem->update($main_file, $content);
     }
 
 
